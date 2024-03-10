406f65043225d73dbf536a30b61c5b58042d06e2
Artem Popov <artem.petrovich001@bk.ru>
2024-03-11 00:56:06 +0300

    one more test

--- parser/parser.py
+++ parser/parser.py
@@ -14,7 +14,7 @@ def extract_links_from_text(text):
   return re.findall(url_extract_pattern, text)
 
 def correct_url(url, found_link):
-    if (found_link is None):
+    if (found_link is None or len(found_link) == 0):
         return None
     if (found_link[0] == '/'):
         return url + found_link
@@ -63,25 +63,25 @@ def get_text_docx(file_path):
         fullText.append(para.text)
     return '\n'.join(fullText)
 
-def get_text_doc(file_path):
-    """Получение текста файла с расширением .doc,
-    для этого файл конвертируется с расширением docx,
-    с помощью библиотеки Spire.Doc,
-    далее используется функция get_text_docx
-    """
-    document = Document()
-    document.LoadFromFile('/content/ex.doc')
-    document.SaveToFile('/content/TEMP.docx', FileFormat.Docx2016)
-    document.Close()
-    return get_text_docx('/content/TEMP.docx')[len('Evaluation Warning: The document was created with Spire.Doc for Python.\n'):]
+# def get_text_doc(file_path):
+#     """Получение текста файла с расширением .doc,
+#     для этого файл конвертируется с расширением docx,
+#     с помощью библиотеки Spire.Doc,
+#     далее используется функция get_text_docx
+#     """
+#     document = Document()
+#     document.LoadFromFile('/content/ex.doc')
+#     document.SaveToFile('/content/TEMP.docx', FileFormat.Docx2016)
+#     document.Close()
+#     return get_text_docx('/content/TEMP.docx')[len('Evaluation Warning: The document was created with Spire.Doc for Python.\n'):]
 
-def get_links_doc(file_path):
-    """Получение ссылок файла с расширением .doc,
-    для этого получается текст файла с помощью get_text_doc,
-    далее используется функция extract_links_from_text для
-    извелечения ссылок из текста
-    """
-    return extract_links_from_text(get_text_doc(file_path))
+# def get_links_doc(file_path):
+#     """Получение ссылок файла с расширением .doc,
+#     для этого получается текст файла с помощью get_text_doc,
+#     далее используется функция extract_links_from_text для
+#     извелечения ссылок из текста
+#     """
+#     return extract_links_from_text(get_text_doc(file_path))
 
 def get_links_pdf(file_path):
     """Получение ссылок файла с расширением pdf
--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -1,10 +1,13 @@
 import pytest
+import docx 
 
+
+from bs4 import BeautifulSoup as bs 
 from parser.parser import extract_links_from_text, \
                           get_links_url,  get_text_url, \
                           get_links_docx, get_text_docx, \
-                          get_links_doc, get_text_doc, \
-                          get_links_pdf, get_text_pdf
+                          get_links_pdf, get_text_pdf, \
+                          get_links_doc, get_text_doc
 
 
 #URL-----------------------------------------------------------------------------------
@@ -20,16 +23,18 @@ def test_extract_links_from_text(test_text, expected_result):
     
 
 #WEBSITE-----------------------------------------------------------------------------------
-test_get_links_url_url1 = 'https://twitter.com/home'
-test_get_links_url_res1 = ['https://help.twitter.com/using-twitter/twitter-supported-browsers', 'https://twitter.com/tos', 'https://twitter.com/privacy', 'https://support.twitter.com/articles/20170514', 'https://legal.twitter.com/imprint.html', 'https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=adsinfo']
+#TODO (собираются не все ссылки с тестовой страницы)
+test_get_links_url_url1 = 'https://x.ai/'
+test_get_links_url_res1 = ['https://x.ai/', 'https://x.ai/', 'https://x.ai//about/', 'https://x.ai//career/', 'https://x.ai//prompt-ide/', 'https://dload-oktatas.educatio.hu/erettsegi/feladatok_2023tavasz_kozep/k_matang_23maj_fl.pdf', 'https://x.ai/model-card/', 'https://x.ai/career/', 'https://boards.greenhouse.io/xai/jobs/4101903007', 'https://x.ai/career/', 'https://grok.x.ai', 'https://x.com/xai', 'https://x.ai//legal/terms-of-service/', 'https://x.ai//legal/privacy-policy/']
 
 @pytest.mark.parametrize("test_url, expected_result", [(test_get_links_url_url1, test_get_links_url_res1)])
 def test_get_links_url(test_url, expected_result):
     assert get_links_url(test_url) == expected_result
 
 #-----------------------------------------------------------------------------------
-test_get_text_url_url1 = 'https://twitter.com/home'
-test_get_text_url_res1 = open('tests/parser/test_files/get_text_url_res1.txt', 'r')
+#TODO (нужно ли собирать код страницы или же всё таки контент?)
+test_get_text_url_url1 = 'https://x.ai/'
+test_get_text_url_res1 = bs(open('tests/parser/test_files/get_text_url_res1.txt', 'r'))
 
 @pytest.mark.parametrize("test_url, expected_result", [(test_get_text_url_url1, test_get_text_url_res1)])
 def test_get_text_url(test_url, expected_result):
@@ -60,6 +65,15 @@ test_get_text_docx_result2 = 'Ijree555g\nhttps://chat.openai.com/'
 def test_get_text_docx(test_path, expected_result):
     assert get_text_docx(test_path) == expected_result
 
+#-----------------------------------------------------------------------------------
+test_docx_bad_path = 'tests/parser/test_files/test/ex.pdf'
+docx_expected_exception = docx.opc.exceptions.PackageNotFoundError
+
+@pytest.mark.parametrize("bad_test_path, expected_exception", [(test_docx_bad_path, docx_expected_exception)])
+def test_get_text_docx_exception(bad_test_path, expected_exception):
+    with pytest.raises(expected_exception):
+        get_text_docx(bad_test_path)
+
 #DOC-----------------------------------------------------------------------------------
 test_get_links_doc_path = 'tests/parser/test_files/ex.doc'
 test_get_links_doc_result = ['https://twitter.com/home', 'https://twitter.com/home', 'https://chat.openai.com/']
@@ -85,6 +99,16 @@ test_get_text_doc_result2 = 'Ijree555g\nhttps://chat.openai.com/'
                                                         (test_get_text_doc_path2, test_get_text_doc_result2)])
 def test_get_text_doc(test_path, expected_result):
     assert get_text_doc(test_path) == expected_result
+
+#-----------------------------------------------------------------------------------
+test_doc_bad_path = 'tests/parser/test_files/test/ex.pdf'
+doc_expected_exception = docx.opc.exceptions.PackageNotFoundError
+
+@pytest.mark.parametrize("test_path, expected_exception", [(test_doc_bad_path, doc_expected_exception)])
+def test_get_text_doc_exception(bad_test_path, expected_exception):
+    with pytest.raises(expected_exception):
+        get_text_doc(bad_test_path)
+
 
 #PDF-----------------------------------------------------------------------------------
 test_get_links_pdf_url = 'tests/parser/test_files/ex.pdf'
