5402a51b63b3c49a1db645606ca4a7c90627df6f
Timur <timur.axmatshin@mail.ru>
2024-04-13 23:56:03 +0300

    vk_api_raw

--- crawl/crawling.py
+++ crawl/crawling.py
@@ -132,8 +132,11 @@ class SiteCrawler:
                 'FEED_FORMAT' : 'csv',
                 'FEED_URI': save_paths[i],
                 'ROBOTSTXT_OBEY': False,
-                'USER_AGENT': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-
+                'DOWNLOADER_MIDDLEWARES': {
+        'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+        'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+        'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+        'scrapy_fake_useragent.middleware.RetryUserAgentMiddleware': 401,},
             }
             processes.append(CrawlerProcess(settings))
         # ут сам процесс
