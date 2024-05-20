9b1829a08468c3aa55645c4901e056366ff4b65b
Artem Popov <artem.petrovich001@bk.ru>
2024-05-20 11:36:29 +0300

    fix

--- inverted_index/deduplication.py
+++ inverted_index/deduplication.py
@@ -1,5 +1,12 @@
-def deduplicate_data(data):
+def deduplicate_data(docs):
     """
-    Дедупликация данных
+    Дедупликация совпадающих документов
     """
-    pass
+    dublication_dict = {}
+    for idx, doc in enumerate(docs):
+        str_doc = str(doc)
+        if str_doc not in dublication_dict.keys():
+            dublication_dict[str_doc] = idx
+        else:
+            docs[str(idx)] = dublication_dict[str_doc]
+    return docs
